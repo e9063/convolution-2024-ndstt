77356700d244d59742d2ff3ac210e66566aeaecd
turbogorogoso <111237093+ndstt@users.noreply.github.com>
2024-10-25 09:04:16 +0000

    comment

--- conv_parallel.c
+++ conv_parallel.c
@@ -22,8 +22,8 @@ int main(){
     int *res = malloc(sizeof(int) * Nres);
 
     // ---- Parallel Convolution ----
-    //omp_set_num_threads(4);
-    double start = omp_get_wtime();
+    omp_set_num_threads(4);
+    //double start = omp_get_wtime();
     #pragma omp parallel for
     for (int i = 0; i < Nres; i++) {
         res[i] = 0;
